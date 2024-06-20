6c9edb29b6bb49bdc4f853f4c299c22d32251e9d
Hana Aschalew <h2anaschalew@gmail.com>
2024-06-21 00:46:46 +0300

    injected additional weather app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@ function refreshWeather(response){
     let temp= document.querySelector("#temp")
     let cityTemp= response.data.temperature.current
     let city= document.querySelector("#city")
+    let timeElement= document.querySelector("#time")
+    let description= document.querySelector("#description")
+    let humidityElement= document.querySelector("#humidity")
+    let windElement= document.querySelector("#wind")
+    let time= new Date(response.data.time * 1000)
+    
     city.innerHTML= response.data.city
     temp.innerHTML= Math.round(cityTemp)
+    timeElement.innerHTML= formatDate(time)
+    description.innerHTML= response.data.condition.description
+    humidityElement.innerHTML= `Humidity: ${response.data.temperature.humidity}%`
+    windElement.innerHTML= `Wind: ${response.data.wind.speed} km/h`
+    console.log(response)
 }
 
+function formatDate(dateString) {
+    let time = new Date(dateString);
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[time.getDay()];
+   
+
+    if (minute < 10 ){
+        minute = "0" + minute;
+    }
+    return `${day} ${hour}:${minute}`;
+}
 function cityInfo(city){
     let apiKey= "ac97dbdbo283f0b8fa3e34tfec52460e"
     let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
@@ -18,7 +42,7 @@ function searchCity(event){
     
     cityInfo(searchInp.value)
 }
-let btn= document.querySelector('.submit-form')
-btn.addEventListener("submit",searchCity )
+let formSubmit= document.querySelector('.submit-form')
+formSubmit.addEventListener("submit",searchCity )
 
 cityInfo("paris")
